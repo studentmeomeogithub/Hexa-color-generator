c2aa3d1070449ad1e74005ad2d6dcb7b48a4814c
Arsalan meo <arsalanmeomeo9@gmail.com>
2024-10-05 15:08:07 +0500

    added new file

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const getColor = () =>{
     const randomNumber = Math.floor(Math.random() * 16777215);
+    console.log(randomNumber)
     const randomCode = "#" + randomNumber.toString(16);
     document.body.style.backgroundColor = randomCode;
     document.getElementById("color-code").innerText = randomCode;
